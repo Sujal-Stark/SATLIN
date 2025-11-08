112ac905bcc83b2846dfe3f3c6c23ec89daaa9b7
Sujal-Stark <sujalstark2010@gmail.com>
2025-11-08 10:59:03 +0530

    Modified to handle both text and image

--- ItemWidget.cpp
+++ ItemWidget.cpp
@@ -7,12 +7,21 @@
 #include<QPixmap>
 #include<iostream>
 
+#include "Constants.h"
+
 using namespace std;
 
 void ItemWidget::mousePressEvent(QMouseEvent *event){
     // identifies the content of item and then send it as signal
     if (event->button() == Qt::MouseButton::LeftButton) {
-        emit this->textItemClickedSignal(this->text().toStdString());
+        if (OBJECT_RECOGNITION_FLAG_ARRAY[Constants::TEXT_SIGNAL_INDEX]) {
+            emit this->textItemClickedSignal(this->text().toStdString());
+            OBJECT_RECOGNITION_FLAG_ARRAY[Constants::TEXT_SIGNAL_INDEX] = false; // reset
+        }
+        else if (OBJECT_RECOGNITION_FLAG_ARRAY[Constants::IMAGE_SIGNAL_INDEX]) {
+            emit this->imageItemClickedSignal(this->pixmap());
+            OBJECT_RECOGNITION_FLAG_ARRAY[Constants::IMAGE_SIGNAL_INDEX] = false; // reset
+        }
     }
 
     // changes the label color
@@ -30,5 +39,12 @@ void ItemWidget::mouseReleaseEvent(QMouseEvent *event) {
         "border-radius: 5px;"
         "background-color: rgba(145, 191, 250, 0);"
     );
+    OBJECT_RECOGNITION_FLAG_ARRAY[OBJECT_RECOGNITION_FLAG_INDEX] = true; // set
 }
+
+void ItemWidget::setObjectType(const int objectFlag) {
+    OBJECT_RECOGNITION_FLAG_ARRAY[objectFlag] = true; // set
+    OBJECT_RECOGNITION_FLAG_INDEX = objectFlag;
+}
+
 
--- ItemWidget.h
+++ ItemWidget.h
@@ -12,10 +12,12 @@ using namespace std;
 
 class ItemWidget : public QLabel{
     Q_OBJECT
+    vector<bool> OBJECT_RECOGNITION_FLAG_ARRAY = {false, false, false, false}; // stores Flag
+    int OBJECT_RECOGNITION_FLAG_INDEX = -1; // stores Flag Index
 
 signals:
-    void textItemClickedSignal(string text);
-    void imageItemClickedSignal(QPixmap image);
+    void textItemClickedSignal(string text); // returns Text item
+    void imageItemClickedSignal(QPixmap image); // returns Image item
 
 protected:
     void mousePressEvent(QMouseEvent *event) override;
@@ -23,4 +25,5 @@ protected:
 
 public:
     explicit ItemWidget(const QString &text, QWidget *parent = nullptr): QLabel(text, parent){};
+    void setObjectType(int objectFlag); // accepts flag
 };
