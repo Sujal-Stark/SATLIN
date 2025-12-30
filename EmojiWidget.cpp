f2a2dd53f86e0f454c378740e3d5f5273c943c3b
Sujal-Stark <sujalstark2010@gmail.com>
2025-12-31 00:25:00 +0530

    testing update

---
+++ EmojiWidget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sujal-stark on 12/29/25.
+//
+
+#include "EmojiWidget.h"
---
+++ EmojiWidget.h
@@ -0,0 +1,13 @@
+//
+// Created by sujal-stark on 12/29/25.
+//
+
+#ifndef SATLIN_EMOJIWIDGET_H
+#define SATLIN_EMOJIWIDGET_H
+
+
+class EmojiWidget {
+};
+
+
+#endif //SATLIN_EMOJIWIDGET_H
