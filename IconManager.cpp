49c400dff2ae8922325e3e66f8d1d18ebb28c0e9
Sujal-Stark <sujalstark2010@gmail.com>
2026-02-04 04:42:10 +0530

    IconManager file is added

---
+++ IconManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sujal-stark on 2/4/26.
+//
+
+#include "IconManager.h"
---
+++ IconManager.h
@@ -0,0 +1,13 @@
+//
+// Created by sujal-stark on 2/4/26.
+//
+
+#ifndef SATLIN_ICONMANAGER_H
+#define SATLIN_ICONMANAGER_H
+
+
+class IconManager {
+};
+
+
+#endif //SATLIN_ICONMANAGER_H
