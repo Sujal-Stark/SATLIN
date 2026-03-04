26ac77c322eb79421799cbf240b21ae772f7a2ac
Sujal-Stark <sujalstark2010@gmail.com>
2026-03-04 14:32:20 +0530

    MimeAnalyzer works with ItemRepository and HashGenerator and textanalyzer is now handling media in better way

--- Util/MimeDataAnalyzer.cpp
+++ Util/MimeDataAnalyzer.cpp
@@ -3,3 +3,156 @@
 //
 
 #include "MimeDataAnalyzer.h"
+
+#include <qurl.h>
+
+#include "Constants.h"
+#include <QStandardPaths>
+#include <chrono>
+
+MimeDataAnalyzer::MimeDataAnalyzer() {
+    createTemporaryFileLocation();
+    this->createConnections();
+};
+
+void MimeDataAnalyzer::createConnections() const {
+    connect(
+        qApp, &QApplication::aboutToQuit,
+        this, &MimeDataAnalyzer::removeTemporaryFiles
+    );
+}
+
+void MimeDataAnalyzer::driverReceiver(const shared_ptr<ItemRepository>& repo) {
+    this->itemRepository = repo;
+}
+
+void MimeDataAnalyzer::analyzeMimeObject(const QMimeData& mime) {
+    if (mime.hasText()) {
+        const string text = mime.text().toStdString();
+        if(!this->analyzeText(text)) {
+            const QString currHash = HashGenerator::generateTextHash(text);
+            if (this->itemRepository->addNewTextItemHash(currHash)) {
+                emit textReleaseSignal(mime.text(), currHash);
+            }
+        }
+    }
+    if (mime.hasImage()) {
+        const QImage image = convertToQImage(mime);
+        if(image.isNull())return;
+        QString fileName = saveImageFile(image);
+        if (!fileName.isNull()) {
+            // emit imageFilePathReleaseSignal(
+            //     fileName, this->DEFAULT_IMAGE_EXTENSION, 1
+            // );
+        }
+    }
+}
+
+QImage MimeDataAnalyzer::convertToQImage(const QMimeData& mime_data) {
+    QVariant imageVariant = mime_data.imageData();
+
+    if (imageVariant.isValid() && imageVariant.canConvert<QImage>()) {
+        return qvariant_cast<QImage>(imageVariant);
+    }
+
+    return {};
+}
+
+bool MimeDataAnalyzer::isValidPath(const filesystem::path& address) {
+    return filesystem::exists(address);
+}
+
+bool MimeDataAnalyzer::isValidURL(const string &text) {
+    const QUrl url = QUrl(QString::fromStdString(text));
+    return url.isValid() && (url.scheme() == Constants::HTTP || url.scheme() == Constants::HTTPS);
+}
+
+
+bool MimeDataAnalyzer::isImageFile(string ext) {
+    ranges::transform(ext, ext.begin(), ::tolower);
+    return ext == Constants::JPG || ext == Constants::JPEG
+    || ext == Constants::PNG || ext == Constants::WEBP || ext == Constants::TIFF;
+}
+
+QString MimeDataAnalyzer::saveImageFile(const QImage& imageData) const {
+    QString fileName = QString::fromStdString(
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation).toStdString()
+        + this->TEMP_IMAGE_PATH + "/IMAGE_" + to_string(timeStamp()) + Constants::PNG
+    );
+    if(imageData.save(fileName))return fileName;
+    return nullptr;
+}
+
+bool MimeDataAnalyzer::isVideoFile(string ext) {
+    ranges::transform(ext, ext.begin(), ::tolower);
+    return ext == Constants::MP4 || ext == Constants::MKV
+    || ext == Constants::AVI || ext == Constants::WEBM;
+}
+
+bool MimeDataAnalyzer::isAudioFile(string ext) {
+    ranges::transform(ext, ext.begin(), ::tolower);
+    return ext == Constants::MP3 || ext == Constants::WAV
+    || ext == Constants::AAC || ext == Constants::FLAC|| ext == Constants::OGG
+    || ext == Constants::WMA || ext == Constants::M4A;
+}
+
+bool MimeDataAnalyzer::analyzeText(const string &text) {
+    stringstream stream = stringstream(text);
+    string line;
+    bool flag = true;
+
+    while (getline(stream, line, '\n')) {
+        if (line.empty())continue;
+        filesystem::path address = filesystem::path(line);
+
+        if (filesystem::exists(address)) {
+            string ext = address.extension().string();
+            QString filePath = QString::fromStdString(line);
+            if (isImageFile(ext)) {
+                // emit imageFilePathReleaseSignal(
+                //     filePath, ext.data(), 0
+                // );
+            }else if (isVideoFile(ext)) {
+                // emit videoFilePathReleaseSignal(
+                //     filePath, ext.data(), 0
+                // );
+            }else if (isAudioFile(ext)) {
+                // emit audioFilePathReleaseSignal(
+                //     filePath, ext.data(), 0
+                // );
+            }else qWarning()<<"Different File detected";
+        }
+        else if (isValidURL(line)) {
+
+        }
+        else flag = false;
+    }
+
+    return flag;
+}
+
+void MimeDataAnalyzer::createTemporaryFileLocation() const {
+    const string base = QStandardPaths::writableLocation(
+        QStandardPaths::AppDataLocation
+    ).toStdString();
+
+    filesystem::create_directories(base + this->TEMP_IMAGE_PATH);
+    filesystem::create_directories(base + this->TEMP_AUDIO_PATH);
+    filesystem::create_directories(base + this->TEMP_VIDEO_PATH);
+}
+
+void MimeDataAnalyzer::removeTemporaryFiles() const {
+    const string base = QStandardPaths::writableLocation(
+        QStandardPaths::AppDataLocation
+    ).toStdString();
+
+    filesystem::remove_all(base + this->TEMP_IMAGE_PATH);
+    filesystem::remove_all(base + this->TEMP_AUDIO_PATH);
+    filesystem::remove_all(base + this->TEMP_VIDEO_PATH);
+}
+
+long long MimeDataAnalyzer::timeStamp() {
+    return chrono::duration_cast<chrono::microseconds>(
+        chrono::system_clock::now().time_since_epoch()
+    ).count();
+}
--- Util/MimeDataAnalyzer.h
+++ Util/MimeDataAnalyzer.h
@@ -4,5 +4,131 @@
 
 #pragma once
 
-class MimeDataAnalyzer {
+#include <QMimeData>
+#include <filesystem>
+#include <QWidget>
+#include <QUrl>
+#include <QApplication>
+
+#include "ItemRepository.h"
+#include "../Util/HashGenerator.h"
+
+using namespace std;
+
+class MimeDataAnalyzer : public QWidget{
+    Q_OBJECT
+    // General
+    shared_ptr<ItemRepository> itemRepository = nullptr;
+    const string TEMP_IMAGE_PATH = "/items/images";
+    const string TEMP_AUDIO_PATH = "/items/audios";
+    const string TEMP_VIDEO_PATH = "/items/videos";
+
+    /**
+     * All the necessary signals are bonded with their corresponding
+     * methods in this createConnections method. Must be used inside
+     * constructor to create necessary connections First.
+     */
+    void createConnections() const;
+
+    /**
+     * Given a filesystem::path variable this method verifies the
+     * existence of the given path in the system.
+     */
+    static bool isValidPath(const filesystem::path& address);
+
+    /**
+     * Given a const string& variable this method verifies if
+     * the string is a Web URL or not.
+     */
+    static bool isValidURL(const string& text);
+
+    /**
+     * This method returns the time stamp in microseconds
+     * at which the item is copied and processed as a long - long
+     * number.
+     */
+    static long long timeStamp();
+
+    /**
+     * This method creates folders to store incoming items as files
+     * inside some specific folders created.
+     */
+    void createTemporaryFileLocation() const;
+
+    /**
+     * Removes all The files from Temporary Locations to save memory.
+     */
+    void removeTemporaryFiles() const;
+
+    // Text
+    /**
+     * This method operates over text items coming from MimeData.
+     * It checks for a text to contain different file's path, web
+     * urls, and sends corresponding signals with meta-data to the
+     * required Class.
+     */
+    [[nodiscard]]static bool analyzeText(const string& text);
+
+    // Image
+    const QString DEFAULT_IMAGE_EXTENSION = ".png";
+    /**
+     * Given an QImage Object (nonempty) this method provides a unique
+     * name and tries to save the image object in specific folder.
+     * The output of the operation is returned.
+     */
+    [[nodiscard]] QString saveImageFile(const QImage& imageData) const;
+
+    /**
+     * Checks if the given string is a valid extension of supported
+     * image file extension or not. Irrespective of case.
+     */
+    [[nodiscard]] static bool isImageFile(string ext);
+
+    // Video
+    /**
+     * Checks if the given string is a valid extension of supported
+     * video file extension or not. Irrespective of case.
+     */
+    [[nodiscard]] static bool isVideoFile(string ext);
+
+    // Audio
+    /**
+     * Checks if the given string is a valid extension of supported
+     * audio file extension or not. Irrespective of case.
+     */
+    [[nodiscard]] static bool isAudioFile(string ext);
+
+    signals:
+    void textReleaseSignal(QString text, QString hash);
+
+    // image
+    void imageFilePathReleaseSignal(QString text, QString ext, int saveStatus);
+    void imageObjectReleaseSignal(QSharedPointer<QImage> qImage);
+
+    // video
+    void videoFilePathReleaseSignal(QString text, QString ext, int saveStatus);
+
+    // audio
+    void audioFilePathReleaseSignal(QString text, QString ext, int saveStatus);
+
+public:
+    MimeDataAnalyzer();
+    /**
+     * It also receives an ItemRepository object and use it for preventing
+     * deduplication
+     */
+    void driverReceiver(const shared_ptr<ItemRepository>& repo);
+
+    /**
+     * it receives a QMimeData Object and operate on it.
+     */
+    void analyzeMimeObject(const QMimeData& mime);
+
+    // Image
+    /**
+     * Accepts the raw image data from the ClipBoard and
+     * tries to convert into QImageObject. If unable to convert
+     * then it returns an empty QImage Object
+     */
+    static QImage convertToQImage(const QMimeData& mime_data);
 };
