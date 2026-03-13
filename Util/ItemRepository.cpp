53070c819991016a02daacea4d7f5f3354ad8ab3
Sujal-Stark <sujalstark2010@gmail.com>
2026-03-14 03:17:25 +0530

    New methods are introduced to handle Imgae containers

--- Util/ItemRepository.cpp
+++ Util/ItemRepository.cpp
@@ -5,21 +5,83 @@
 #include "ItemRepository.h"
 #include <QDebug>
 
-ItemRepository::ItemRepository() : textHashCollector(new QSet<QString>()) {
+ItemRepository::ItemRepository() = default;
 
+ItemRepository::~ItemRepository() {
+    // TEXT
+    this->textHashCollector.clear();
+
+    // IMAGE
+    qDeleteAll(this->imageHashCollector);
+    this->imageHashCollector.clear();
 }
 
-bool ItemRepository::addNewTextItemHash(const QString &textHash) const {
+bool ItemRepository::addNewTextItemHash(const QString &textHash) {
     if (textHash.isNull())return  false;
-    if (this->textHashCollector->contains(textHash)) return false;
-    this->textHashCollector->insert(textHash);
+    if (this->textHashCollector.contains(textHash)) return false;
+    this->textHashCollector.insert(textHash);
     return true;
 }
 
 bool ItemRepository::containsTextHash(const QString &textHash) const {
-    return this->textHashCollector->contains(textHash);
+    return this->textHashCollector.contains(textHash);
 }
 
-bool ItemRepository::removeTextItemHash(const QString &textHash) const {
-    return this->textHashCollector->remove(textHash);
+bool ItemRepository::removeTextItemHash(const QString &textHash) {
+    return this->textHashCollector.remove(textHash);
+}
+
+bool ItemRepository::addNewImageItem(
+    const QString &imageHash, const QString &filePath, const QString &extension,
+    const int saveStatus, const QString &timeStamp
+) {
+    if (imageHash.isEmpty() || filePath.isEmpty() || extension.isEmpty()) return false;
+    if (saveStatus > 1 || saveStatus < 0)return  false;
+    if (timeStamp.isEmpty())return false;
+
+    if (this->imageHashExists(imageHash))return false;
+
+    this->imageHashCollector.insert(
+        imageHash, new ImageContainer(
+            saveStatus, filePath, extension, timeStamp
+        )
+    );
+
+    return true;
+}
+
+bool ItemRepository::imageHashExists(const QString& imageHash) {
+    const QMap<QString, ImageContainer*>::iterator it = this->imageHashCollector.find(imageHash);
+    return it != this->imageHashCollector.end();
+}
+
+bool ItemRepository::removeImageItemHash(const QString &imageHash) {
+    const QMap<QString, ImageContainer*>::iterator it = this->imageHashCollector.find(imageHash);
+    if (it == this->imageHashCollector.end())return false;
+    delete it.value();
+    this->imageHashCollector.erase(it);
+    return true;
+}
+
+const ImageContainer* ItemRepository::getImageContainer(const QString& imageHash) {
+    if (imageHash.isEmpty())return nullptr;
+
+    const QMap<QString, ImageContainer*>::iterator it = this->imageHashCollector.find(imageHash);
+    if (it == this->imageHashCollector.end())return nullptr;
+
+    return it.value();
+}
+
+void ItemRepository::showImageContainer() const {
+    qDebug()<<"Showing current Image Hashes....";
+    for (const auto& it : this->imageHashCollector.keys()) {
+        qDebug()<<it;
+    }
+}
+
+void ItemRepository::showTextContainer() const {
+    qDebug()<<"Showing Current Text Hashes....";
+    for (const auto& it : this->textHashCollector) {
+        qDebug()<<it;
+    }
 }
--- Util/ItemRepository.h
+++ Util/ItemRepository.h
@@ -4,34 +4,106 @@
 
 #pragma once
 
+#include <qmap.h>
 #include <qscopedpointer.h>
 #include <QString>
 #include <QSet>
 
+#include "DataModels.h"
+
 using namespace std;
 
 class ItemRepository {
-    QScopedPointer<QSet<QString>> textHashCollector;
+    /**
+     * Stores The TextItem's Hash value for redundancy checking.
+     */
+    QSet<QString> textHashCollector;
+
+    /**
+     * @brief Stores metaData Corresponding to image items via it's hash value.
+     * The meta used: file path(QString), extension(QString), saveStatus(int),
+     * timeStamp(QString). These metadata are wrapped inside ImageContainer
+     * data model.
+     */
+    QMap<QString, ImageContainer*> imageHashCollector;
 
 public:
     ItemRepository();
 
     /**
-     * Adds the Hash values of text item into the CollectorSet only if
+     * @brief Removes All logged information about media when Application is closed.
+     * to avoid temporary files after use.
+     */
+    ~ItemRepository();
+
+    // Text
+    /**
+     * @brief Adds the Hash values of text item into the CollectorSet only if
      * the Hash Value is valid and not empty. Other-wise it returns False
      * as Failed in adding.
+     * @param textHash Hexadecimal Hash value to uniquely identify a Text.
      */
-    [[nodiscard]] bool addNewTextItemHash(const QString& textHash) const;
+    [[nodiscard]] bool addNewTextItemHash(const QString& textHash);
 
     /**
-     * Given a textHash (const QString&) this method checks if repository
+     * @brief Given a textHash (const QString&) this method checks if repository
      * already contains this hash value or not.
      */
     [[nodiscard]] bool containsTextHash(const QString& textHash) const;
 
     /**
-     * Given a textHash(const QString&) this method removes the text hash
+     * @brief Given a textHash(const QString&) this method removes the text hash
      * value from the repository if it is found.
      */
-    [[nodiscard]] bool removeTextItemHash(const QString& textHash) const;
+    [[nodiscard]] bool removeTextItemHash(const QString& textHash);
+
+    // Image
+    /**
+     * @brief Checks either the parameters are valid or not and saves the information
+     * as ImageContainer(DataModels.h) Structure. If the ImageHash is already present
+     * in the repository then it can't be added again.
+     * @param imageHash Hexadecimal hash of the image file
+     * @param filePath Stores temporary location of image file
+     * @param extension Stores image extension [available in Constants]
+     * @param saveStatus stores either the image is temporarily saved or permanently.
+     * @param  timeStamp The time at which the image is copied into the clipboard.
+     * Format -> HH:MM:SS:DD:MM:YYYY
+     */
+    [[nodiscard]] bool addNewImageItem(
+        const QString& imageHash, const QString& filePath, const QString& extension,
+        int saveStatus, const QString& timeStamp
+    );
+
+    /**
+     * @brief Check's validity of imageHash parameter. If found valid then
+     * manually deletes the imageContainer(DataModels.h) structure and
+     * removes the corresponding hash value.
+     * @param imageHash Hexadecimal hash of the image file.
+     */
+    [[nodiscard]] bool removeImageItemHash(const QString& imageHash);
+
+    /**
+     * @brief Checks if the Given hash value exists or not in the Corresponding
+     * Map Object.
+     * @param imageHash Hexadecimal hash value of Image Object.
+     */
+    [[nodiscard]] bool imageHashExists(const QString& imageHash);
+
+    /**
+     * @brief Checks validity of ImageHash. If valid then returns the container pointer.
+     * @param imageHash Hexadecimal hash value of Image Object.
+     */
+    [[nodiscard]] const ImageContainer* getImageContainer(const QString& imageHash);
+
+    /**
+     * @brief primarily used for debugging. It prints how many hash values are present
+     * inside QMap.
+     */
+    void showImageContainer() const;
+
+    /**
+     * @brief Primarily used for debugging. It prints how many hash values are present
+     * inside QSet.
+     */
+    void showTextContainer() const;
 };
