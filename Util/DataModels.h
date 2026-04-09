2fa26b4284f0b7fbec943dadc494614e25dea47a
Sujal-Stark <sujalstark2010@gmail.com>
2026-04-09 16:57:44 +0530

    new structure AudioContainer is added to store Audio File into itemRepository

--- Util/DataModels.h
+++ Util/DataModels.h
@@ -10,11 +10,28 @@
  * used for clubbing all necessary information of Image all together.
  * @param saveStatus Expresses either a ImageFile is stored temporarily or permanently
  * @param filePath System Address of the Image File.
- * @param extension The Default extension of Image.
+ * @param extension The extension of Image.
  * @param timeStamp Time it's copied.
  */
 struct ImageContainer {
     int saveStatus;
+    QString filePath;
+    QString extension;
+    QString timeStamp;
+};
+
+/**
+ * @brief This Structure stores the meta-data of an Audio file copied or address based.
+ * Used for clubbing all necessary information of audio file all together.
+ * @param saveStatus Expresses either an audio file stored permanently or temporarily.
+ * @param fileSize The Kilobyte size of audio File.
+ * @param filePath System Address of the audio File.
+ * @param extension The extension of audio.
+ * @param timeStamp Time it's copied.
+ */
+struct AudioContainer {
+    int saveStatus;
+    qint32 fileSize;
     QString filePath;
     QString extension;
     QString timeStamp;
