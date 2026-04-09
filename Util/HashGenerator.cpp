23b0fbf46fdbe3bb4339efb331f0f51fd7feda12
Sujal-Stark <sujalstark2010@gmail.com>
2026-04-09 17:04:11 +0530

    New Method is created to calculate hash values of AudioFile

--- Util/HashGenerator.cpp
+++ Util/HashGenerator.cpp
@@ -4,6 +4,8 @@
 
 #include "HashGenerator.h"
 #include <QCryptographicHash>
+#include <QFile>
+#include <QFileInfo>
 
 QString HashGenerator::generateTextHash(const QString &text) {
     if (text.isNull())return nullptr;
@@ -42,4 +44,37 @@ QString HashGenerator::generateImageObjectHash(const QImage &imageObject) {
 
     return QString("%1_%2_%3_%4").arg(imageObject.width())
     .arg(imageObject.height()).arg(size).arg(partialHash);
+}
+
+
+QString HashGenerator::generateAudioObjectHash(const QString &address) {
+    QFile file = QFile(address);
+    file.open(QIODevice::ReadOnly);
+
+    const QFileInfo info = QFileInfo(address);
+    const qint32 fileSize = file.size();
+
+    const qint32 chunkSize = qMin(100 * 1024, fileSize/3);
+
+    auto SHA_256 = [](const QByteArray& data) -> QByteArray {
+        return QCryptographicHash::hash(data, QCryptographicHash::Sha256);
+    };
+
+    file.seek(0);
+    const QByteArray first4 = file.read(4);
+    const QByteArray hash1 = SHA_256(file.read(chunkSize));
+
+    file.seek(fileSize/2 - chunkSize/2);
+    const QByteArray hash2 = SHA_256(file.read(chunkSize));
+
+    file.seek(fileSize - chunkSize);
+    const QByteArray hash3 = SHA_256(file.read(chunkSize));
+
+    file.seek(fileSize - 4);
+    const QByteArray hash4 = SHA_256(first4 + file.read(4));
+
+    const QByteArray finalHash = SHA_256(hash1 + hash2 + hash3 + hash4);
+
+    return QString("%1_%2_%3").arg(finalHash.toHex()).arg(QString::number(fileSize))
+    .arg(QString::number(info.birthTime().toMSecsSinceEpoch()));
 }
--- Util/HashGenerator.h
+++ Util/HashGenerator.h
@@ -21,7 +21,7 @@ public:
 
     /**
      * @brief generates a SHA256 hash value based some chunks from the
-     * image itself and combine it's meta-data like byteSize, width,
+     * image itself and combine its metadata like byteSize, width,
      * height as additional safety.
      * @param imagePath file path of the image.
      */
@@ -29,9 +29,17 @@ public:
 
     /**
      * @brief generates a SHA256 hash value based some chunks from the
-     * image itself and combine it's meta-data like byteSize, width,
+     * image itself and combine its metadata like byteSize, width,
      * height as additional safety.
      * @param imageObject Image Object.
      */
     static QString generateImageObjectHash(const QImage& imageObject);
+
+    /**
+     * @brief Generates a SHA256 hash value based on first mid and last chunks
+     * of 100KB size of Audio, and combines with metadata like byteSize, micro
+     * seconds time from epoch for additional safety.
+     * @param address File path for the Audio File
+     */
+    static QString generateAudioObjectHash(const QString& address);
 };
