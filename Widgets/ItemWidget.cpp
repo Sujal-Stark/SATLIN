0eec24eda36563a608b98cc92cc1234743aecd5b
Sujal-Stark <sujalstark2010@gmail.com>
2026-04-15 16:59:02 +0530

    ItemWidget is changed from text container to a virtual class

--- Widgets/ItemWidget.cpp
+++ Widgets/ItemWidget.cpp
@@ -10,8 +10,6 @@
 #include "../Ui/ClipBoardInterface.h"
 #include "../Util/Constants.h"
 
-using namespace std;
-
 ItemWidget::ItemWidget() {
     this->setLayout(this->masterLayout);
     this->setFixedWidth(Constants::ITEM_WIDGET_WIDTH); // for constant width
@@ -76,96 +74,5 @@ void ItemWidget::establishConnections() {
     connect(
         this->editButton, &QPushButton::clicked,
         this, &ItemWidget::editButtonClicked
-    );
-}
-
-void ItemWidget::setTextManagerInterfaceInputs(
-    const shared_ptr<TextManagerInterface>& interface,
-    const shared_ptr<ItemRepository>& repo
-    ) {
-    this->textManagerInterface = interface;
-    this->textManagerInterface->assignDrivers(repo);
-}
-
-void ItemWidget::assignText(const QString &text, const QString& textHash) {
-    this->textLabel = TextManagerInterface::generateNewTextLabel(text, textHash);
-
-    if (this->textLabel != nullptr) {
-        this->textLabel->show();
-        this->contentHolder->addWidget(this->textLabel, Qt::AlignmentFlag::AlignCenter);
-    }
-}
-
-void ItemWidget::mousePressEvent(QMouseEvent *event){
-    if (event->button() == Qt::MouseButton::LeftButton) {
-        emit this->textItemClickedSignal(this->textLabel->text());
-    }
-
-    this->setStyleSheet(
-        "border: 1px solid white;"
-        "border-radius: 5px;"
-        "background-color: rgba(54, 54, 54, 150);"
-    );
-}
-
-void ItemWidget::mouseReleaseEvent(QMouseEvent *event) {
-    this->setStyleSheet(
-        "border: 0px solid white;"
-        "border-radius: 5px;"
-        "background-color: rgba(15, 14, 14, 150);"
-    );
-}
-
-void ItemWidget::deleteButtonClicked() {
-    if (
-        this->textManagerInterface->removeItem(
-            this->textLabel->property(Constants::SHA_STRING_KEY).toString()
-        )
-    ) {
-        emit this->clipboardCleanSignal();
-        this->deleteLater();
-    }else qWarning()<<"Unable to delete this textItem";
-}
-
-void ItemWidget::saveButtonClicked() {
-    const QString fileName = QFileDialog::getSaveFileName(
-        this,Constants::SAVE_FILE_LABEL, QDir::homePath(),
-    "Text Files (*);;All Files (*)"
-    );
-
-    if (fileName.isEmpty())return;
-
-    QFile file = QFile(fileName);
-    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        QTextStream stream = QTextStream(&file);
-        stream<<this->textLabel->text();//The content is dumped.
-        file.close();
-    }
-}
-
-void ItemWidget::editButtonClicked() {
-    connect(
-        &*this->textManagerInterface, &TextManagerInterface::textEditedSignal,
-        this, &ItemWidget::editedTextReceivedAction
-    );
-
-    this->textManagerInterface->editOnText(this->textLabel->text());
-}
-
-void ItemWidget::editedTextReceivedAction(const QString& editedText) {
-    if (!editedText.isNull()) {
-        const QString oldHash = this->textLabel->property(Constants::SHA_STRING_KEY).toString();
-        const QString newHash = HashGenerator::generateTextHash(editedText);
-        if (this->textManagerInterface->replaceHash(oldHash, newHash)) {
-            this->textLabel->setText(editedText);
-            this->textLabel->setProperty(Constants::SHA_STRING_KEY, newHash);
-            emit textItemClickedSignal(this->textLabel->text());
-            this->update();
-        }
-    }
-
-    disconnect(
-        &*this->textManagerInterface, &TextManagerInterface::textEditedSignal,
-        this, &ItemWidget::editedTextReceivedAction
     );
 }
--- Widgets/ItemWidget.h
+++ Widgets/ItemWidget.h
@@ -14,12 +14,10 @@
 #include "../ManagerSources/TextManagerInterface.h"
 #include "../Util/ItemRepository.h"
 
-using namespace std;
-
 class ItemWidget : public QWidget{
     Q_OBJECT
-    shared_ptr<TextManagerInterface> textManagerInterface = nullptr;
 
+protected:
     // QButtons
     QPushButton *deleteButton = new QPushButton();
     QPushButton *editButton = new QPushButton();
@@ -52,73 +50,23 @@ class ItemWidget : public QWidget{
      * Sends a Signal to ClipBoardInterface to release clipboard content.
      * Set this widget for deletion.
      */
-    void deleteButtonClicked();
+    virtual void deleteButtonClicked() = 0;
 
     /**
      * Expects a valid filePath and text content. This method dump the content as Text
      * into the file. and Saves it. If the file already exists then this method
      * removes all the previous content.
      */
-    void saveButtonClicked();
+    virtual void saveButtonClicked() = 0;
 
     /**
      * Sends the current text from text label to the TextManagerInterface.
      * Establish connection with textEditedSignal of TextManagerInterface
      * with editTextReceivedAction.
      */
-    void editButtonClicked();
-
-    /**
-     * Receives edited Text from TextManagerInterface. Removes the old hash value
-     * that textLabel possess and stores the new hash value. A signal is sent to
-     * clipboard interface to set the edited text as clipboard content. disconnects
-     * signals from TextEditorInterface with this method.
-     */
-    void editedTextReceivedAction(const QString& editedText);
-
-signals:
-    /**
-     * Sends the TextLabel's content to the ClipBoardInterface when the
-     * widget is clicked.
-     */
-    void textItemClickedSignal(QString text);
-
-    /**
-     * Sends a signal to clipboardInterface to release the Clipboard's
-     * content.
-     */
-    void clipboardCleanSignal();
-
-protected:
-    /**
-     * As the Widget is Clicked this method made changes in Widget's style.
-     * Sends a signal to ClipBoardInterface to set current item in clipBoard.
-     */
-    void mousePressEvent(QMouseEvent *event) override;
-
-    /**
-     * Reverts the style to default as the press is released.
-     */
-    void mouseReleaseEvent(QMouseEvent *event) override;
+    virtual void editButtonClicked() = 0;
 
 public:
-    explicit ItemWidget();
+    ItemWidget();
     void construct() const;
-
-    /**
-     * Creates a textLabel from the given Text and show the textLabel
-     * in the UI.
-     */
-    void assignText(const QString& text, const QString& textHash); // holds text
-
-    /**
-     * Accepts Drivers Like TextManagerInterface and ItemRepository.
-     * Saves TextManagersInterface's pointer and assign ItemRepository
-     * to the TextManagerInterface. Connect with TextManagerInterface's
-     * signal.
-     */
-    void setTextManagerInterfaceInputs(
-        const shared_ptr<TextManagerInterface>& interface,
-        const shared_ptr<ItemRepository>& repo
-    );
 };
