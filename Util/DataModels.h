d0966547d890d9b0484c264fef66d3c6d6a350a8
Sujal-Stark <sujalstark2010@gmail.com>
2026-09-06 18:07:29 +0530

    A new TextContainer is added and a new operator<< overload is added to print that TextContainer value

--- Util/DataModels.h
+++ Util/DataModels.h
@@ -4,6 +4,29 @@
 
 #pragma once
 #include <QString>
+#include <QDebug>
+
+struct TextContainer {
+    QString text;
+    int saveStatus;
+    qint32 textSize;
+    QString extension;
+    QString timeStamp;
+};
+
+inline QDebug operator<<(QDebug debug, const TextContainer* container) {
+    if (container) {
+        debug << "TextContainer["
+        << " Text: " + container->text
+        << " extension: " + container->extension
+        << " save status: " + QString::fromStdString(std::to_string(container->saveStatus))
+        << " size: " + QString::fromStdString(std::to_string(container->textSize)) << " kb"
+        << " time stamp: " + container->timeStamp
+        << " ]";
+    }else debug<<"NULL";
+
+    return debug;
+}
 
 /**
  * @brief This Structure Stores The metadata of an Image File Copied or address based.
@@ -15,13 +38,14 @@
  */
 struct ImageContainer {
     int saveStatus;
+    qint32 fileSize;
     QString filePath;
     QString extension;
     QString timeStamp;
 };
 
 /**
- * @brief This Structure stores the meta-data of an Audio file copied or address based.
+ * @brief This Structure stores the metadata of an Audio file copied or address based.
  * Used for clubbing all necessary information of audio file all together.
  * @param saveStatus Expresses either an audio file stored permanently or temporarily.
  * @param fileSize The Kilobyte size of audio File.
